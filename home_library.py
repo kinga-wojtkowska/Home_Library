b8d3020b180bf62d1ae419bca746423aef454cd8
Kinga Wojtkowska <kinga.wolynska@gmail.com>
2020-10-08 00:41:41 +0200

    check PEP8

--- home_library.py
+++ home_library.py
@@ -1,11 +1,12 @@
 from app import app, db
 from app.models import Book, Author, Borrowing
 
+
 @app.shell_context_processor
 def make_shell_context():
-   return {
+    return {
        "db": db,
        "Book": Book,
        "Author": Author,
        "Borrowing": Borrowing
-   }
+    }
