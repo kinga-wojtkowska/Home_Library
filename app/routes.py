80eff7098f9ecadfadde31b734e662b5bc87b3da
Kinga Wojtkowska <kinga.wolynska@gmail.com>
2020-10-14 21:53:19 +0200

    render to redirect, shorten route address, use new form method

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,13 @@
-from flask import Flask, jsonify, abort, make_response, request, render_template  # noqa: E501
+from flask import Flask, jsonify, abort, make_response, request, render_template, redirect, url_for  # noqa: E501
 from app import db, app
 from app.models import Book, Author, Borrowing
 from app.forms import BooksLibForm, AuthorLibForm, DeleteForm, BorrowingForm
 
 
 # wyświetla wszystkie książki w bazie lub tworzy nową pozycję książkową
-@app.route("/sql/books/", methods=["GET", "POST"])
-def books_list_sql():
-    form = BooksLibForm()
+@app.route("/books/", methods=["GET", "POST"])
+def books_list():
+    form = BooksLibForm.new()
     books_all = Book.query.all()
     if request.method == "POST":
         if form.validate_on_submit():
@@ -18,13 +18,13 @@ def books_list_sql():
             author1.books.append(book1)
             db.session.commit()
             books_all = Book.query.all()
-            return render_template("books.html", books=books_all, form=form)
+            return redirect(url_for("books_list"))
     return render_template("books.html", books=books_all, form=form)
 
 
 # wyświetla wszystkich autorów w bazie lub tworzy nową pozycję autora
-@app.route("/sql/authors/", methods=["GET", "POST"])
-def authors_list_sql():
+@app.route("/authors/", methods=["GET", "POST"])
+def authors_list():
     form = AuthorLibForm()
     authors = Author.query.all()
     if request.method == "POST":
@@ -33,13 +33,13 @@ def authors_list_sql():
             db.session.add(author1)
             db.session.commit()
             authors = Author.query.all()
-            return render_template("authors.html", authors=authors, form=form)
+            return redirect(url_for("authors_list"))
     return render_template("authors.html", authors=authors, form=form)
 
 
-@app.route("/sql/borrowing/", methods=["GET", "POST"])
-def borrowing_list_sql():
-    form = BorrowingForm()
+@app.route("/borrowing/", methods=["GET", "POST"])
+def borrowing_list():
+    form = BorrowingForm.new()
     form2 = DeleteForm()
     borrowing = Borrowing.query.all()
     books = Book.query.all()
@@ -50,17 +50,17 @@ def borrowing_list_sql():
             db.session.add(borrow)
             db.session.commit()
             borrowing = Borrowing.query.all()
-            return render_template("borrowing.html", borrowing=borrowing, form=form, form2=form2)  # noqa: E501
+            return redirect(url_for("borrowing_list"))
         if form2.validate_on_submit():
             borrow = Borrowing.query.filter_by(id=form2.id.data).first()
             borrow.borrowed = False
             db.session.commit()
-            return render_template("borrowing.html", borrowing=borrowing, form=form, form2=form2)  # noqa: E501
+            return redirect(url_for("borrowing_list"))
     return render_template("borrowing.html", borrowing=borrowing, form=form, books=books, form2=form2)  # noqa: E501
 
 
 # pokazuje konkretny rekord z bazy danych, pozwala na zmianę danych lub usunięcie książki  # noqa: E501
-@app.route("/sql/books/<int:book_id>", methods=["GET", "POST"])
+@app.route("/books/<int:book_id>", methods=["GET", "POST"])
 def get_book(book_id):
     form = BooksLibForm()
     book = Book.query.get(book_id)
@@ -72,11 +72,11 @@ def get_book(book_id):
         author1 = Author.query.filter_by(name=form.author.data).first()
         author1.books.append(book)
         db.session.commit()
-        return render_template("book.html", book=book, form=form, book_id=book_id)  # noqa: E501
+        return redirect(url_for("get_book"))
     return render_template("book.html", book=book, form=form, book_id=book_id)
 
 
-@app.route("/sql/authors/<int:author_id>", methods=["GET", "POST"])
+@app.route("/authors/<int:author_id>", methods=["GET", "POST"])
 def get_authors_books(author_id):
     form = DeleteForm()
     author = Author.query.get(author_id)
@@ -87,12 +87,12 @@ def get_authors_books(author_id):
         book = Book.query.filter_by(id=form.id.data).first()
         author.books.remove(book)
         db.session.commit()
-        return render_template("author_books.html", author=author.name, books=books, author_id=author_id, form=form)  # noqa: E501
+        return redirect(url_for("get_authors_books"))
     return render_template("author_books.html", author=author.name, books=books, author_id=author_id, form=form)  # noqa: E501
 
 
 # usuwa rekord książki o danym id z bazy danych
-@app.route("/sql/books/delete/<int:book_id>", methods=['GET'])
+@app.route("/books/delete/<int:book_id>", methods=['GET'])
 def delete_book(book_id):
     book = Book.query.get(book_id)
     db.session.delete(book)
@@ -101,7 +101,7 @@ def delete_book(book_id):
 
 
 # usuwa rekord autora z bazy danych
-@app.route("/sql/authors/delete/<int:author_id>", methods=['GET'])
+@app.route("/authors/delete/<int:author_id>", methods=['GET'])
 def delete_author(author_id):
     author = Author.query.get(author_id)
     db.session.delete(author)
